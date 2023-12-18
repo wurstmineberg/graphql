6264a20010f9f1c9960eb3a5741263b63664bc65
Fenhl <fenhl@fenhl.net>
2023-12-18 03:06:15 +0000

    deploy: Include git push; Build on Debian

--- assets/deploy.ps1
+++ assets/deploy.ps1
@@ -5,7 +5,10 @@ function ThrowOnNativeFailure {
     }
 }
 
-wsl cargo build --release
+git push
+ThrowOnNativeFailure
+
+wsl --distribution debian-m2 cargo build --release
 ThrowOnNativeFailure
 
 scp .\target\release\wurstmineberg-graphql wurstmineberg@wurstmineberg.de:/opt/wurstmineberg/bin/wurstmineberg-graphql
--- assets/deploy.sh
+++ assets/deploy.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+git push
 cargo build --release
 scp target/release/wurstmineberg-graphql wurstmineberg@wurstmineberg.de:/opt/wurstmineberg/bin/wurstmineberg-graphql
 ssh wurstmineberg@wurstmineberg.de /opt/wurstmineberg/bin/wurstmineberg-graphql
