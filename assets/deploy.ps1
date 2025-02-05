4d23f345cda80ad4eef4e53bb0c9e6cbef1c5cfc
Fenhl <fenhl@fenhl.net>
2025-02-05 12:06:18 +0000

    Upgrade dependencies and crate config

--- assets/deploy.ps1
+++ assets/deploy.ps1
@@ -11,8 +11,11 @@ ThrowOnNativeFailure
 wsl --distribution debian-m2 cargo build --release
 ThrowOnNativeFailure
 
+ssh wurstmineberg.de sudo systemctl stop wmb-graphql
+ThrowOnNativeFailure
+
 scp .\target\release\wurstmineberg-graphql wurstmineberg@wurstmineberg.de:/opt/wurstmineberg/bin/wurstmineberg-graphql
 ThrowOnNativeFailure
 
-ssh wurstmineberg@wurstmineberg.de /opt/wurstmineberg/bin/wurstmineberg-graphql
+ssh wurstmineberg.de sudo systemctl start wmb-graphql
 ThrowOnNativeFailure
--- assets/deploy.sh
+++ assets/deploy.sh
@@ -4,5 +4,6 @@ set -e
 
 git push
 cargo build --release
+ssh wurstmineberg.de sudo systemctl stop wmb-graphql
 scp target/release/wurstmineberg-graphql wurstmineberg@wurstmineberg.de:/opt/wurstmineberg/bin/wurstmineberg-graphql
-ssh wurstmineberg@wurstmineberg.de /opt/wurstmineberg/bin/wurstmineberg-graphql
+ssh wurstmineberg.de sudo systemctl start wmb-graphql
