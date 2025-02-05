5b7502098fa2324bd482c16d0cce6b6bf8c5b13d
Fenhl <fenhl@fenhl.net>
2025-02-05 12:16:56 +0000

    Fix deploy

--- assets/deploy.ps1
+++ assets/deploy.ps1
@@ -1,21 +1,48 @@
-function ThrowOnNativeFailure {
-    if (-not $?)
-    {
-        throw 'Native Failure'
-    }
+git push
+if (-not $?)
+{
+    throw 'Native Failure'
 }
 
-git push
-ThrowOnNativeFailure
+# copy the tree to the WSL file system to improve compile times
+wsl --distribution debian-m2 rsync --delete -av /mnt/c/Users/fenhl/git/github.com/wurstmineberg/graphql/stage/ /home/fenhl/wslgit/github.com/wurstmineberg/graphql/ --exclude target
+if (-not $?)
+{
+    throw 'Native Failure'
+}
 
-wsl --distribution debian-m2 cargo build --release
-ThrowOnNativeFailure
+wsl --distribution debian-m2 env -C /home/fenhl/wslgit/github.com/wurstmineberg/graphql cargo build --release --target=x86_64-unknown-linux-musl
+if (-not $?)
+{
+    throw 'Native Failure'
+}
+
+wsl --distribution debian-m2 mkdir -p /mnt/c/Users/fenhl/git/github.com/wurstmineberg/graphql/stage/target/wsl/release
+if (-not $?)
+{
+    throw 'Native Failure'
+}
+
+wsl --distribution debian-m2 cp /home/fenhl/wslgit/github.com/wurstmineberg/graphql/target/x86_64-unknown-linux-musl/release/wurstmineberg-graphql /mnt/c/Users/fenhl/git/github.com/wurstmineberg/graphql/stage/target/wsl/release/wurstmineberg-graphql
+if (-not $?)
+{
+    throw 'Native Failure'
+}
 
 ssh wurstmineberg.de sudo systemctl stop wmb-graphql
-ThrowOnNativeFailure
+if (-not $?)
+{
+    throw 'Native Failure'
+}
 
-scp .\target\release\wurstmineberg-graphql wurstmineberg@wurstmineberg.de:/opt/wurstmineberg/bin/wurstmineberg-graphql
-ThrowOnNativeFailure
+scp .\target\wsl\release\wurstmineberg-graphql wurstmineberg@wurstmineberg.de:/opt/wurstmineberg/bin/wurstmineberg-graphql
+if (-not $?)
+{
+    throw 'Native Failure'
+}
 
 ssh wurstmineberg.de sudo systemctl start wmb-graphql
-ThrowOnNativeFailure
+if (-not $?)
+{
+    throw 'Native Failure'
+}
